d315869d359a60ae33257f6731dc6fa30b41da4c
mika <wursthorn.mika@gmail.com>
2026-02-12 14:27:10 +0100

    feat: add register

---
+++ app/api/auth/register/route.ts
@@ -0,0 +1,102 @@
+/**
+ * Fichier de routes pour l'authentification
+ * Ce fichier gère la route d'inscription (POST)
+ * Il utilise les règles de validation définies dans rules.json pour valider les données d'entrée.
+ */
+
+// Importation des règles de validation et des interfaces nécessaires
+import rules from '@/shared/rules.json';
+import {IRegistrationData, Iuser} from '@/shared/interfaces';
+import {NextResponse} from "next/server";
+import bcrypt from 'bcryptjs';
+import clientPromise from "@/lib/mongodb";
+
+export async function POST(request: Request) {
+    try {
+        // Récupération des données d'inscription depuis le corps de la requête
+        const data: IRegistrationData = await request.json();
+
+        // Vérification que tous les champs requis sont présents
+        if (!data.username || !data.email || !data.password || !data.confirmPassword) {
+            return NextResponse.json({error: "Tous les champs sont requis"}, {status: 400});
+        }
+
+        /**
+         * Validation des données d'inscription en utilisant les règles définies dans rules.json
+         * Chaque champ (username, email, password) est validé en utilisant une expression régulière définie dans les règles.
+         * Si une validation échoue, on utilise le message d'erreur correspondant défini dans les règles
+         * et on retourne une réponse JSON avec un statut 400 (Bad Request).
+         */
+        const usernameRules = rules.rules.users.username;
+        const usernameRegex = new RegExp(usernameRules.pattern);
+        const usernameError = rules.rules.users.username.error;
+
+        const emailRules = rules.rules.users.email;
+        const emailRegex = new RegExp(emailRules.pattern);
+        const emailError = rules.rules.users.email.error;
+
+        const passwordRules = rules.rules.users.password;
+        const passwordRegex = new RegExp(passwordRules.pattern);
+        const passwordError = rules.rules.users.password.error;
+
+        const confirmPasswordError = rules.rules.users.confirmPassword.error;
+
+        // Validation des données d'inscription
+        if (!usernameRegex.test(data.username)) {
+            return NextResponse.json({error: usernameError}, {status: 400});
+        }
+        if (!emailRegex.test(data.email)) {
+            return NextResponse.json({error: emailError}, {status: 400});
+        }
+        if (!passwordRegex.test(data.password)) {
+            return NextResponse.json({error: passwordError}, {status: 400});
+        }
+        if (data.password !== data.confirmPassword) {
+            return NextResponse.json({error: confirmPasswordError}, {status: 400});
+        }
+
+        // Création de l'objet utilisateur final à partir des données d'inscription validées
+        // en ajoutant les champs createdAt et updatedAt et en supprimant le champ confirmPassword qui n'est pas nécessaire
+        const finalData: Iuser = {
+            username: data.username,
+            email: data.email,
+            password: data.password,
+            createdAt: new Date(),
+            updatedAt: new Date()
+        };
+
+        // verification que l'utilisateur et l'email n'existent pas déjà dans la base de données
+        const client = await clientPromise;
+        const db = client.db();
+        const existingUser = await db.collection('users').findOne({$or: [{username: data.username}]});
+        if (existingUser) {
+            return NextResponse.json({error: "Le nom d'utilisateur existe déjà"}, {status: 400});
+        }
+        const existingEmail = await db.collection('users').findOne({$or: [{email: data.email}]});
+        if (existingEmail) {
+            return NextResponse.json({error: "L'email existe déjà"}, {status: 400});
+        }
+
+        /**
+         * Hashage du mot de passe en utilisant bcryptjs pour sécuriser les données de l'utilisateur
+         * avant de les stocker dans la base de données.
+         * npm install bcryptjs
+         * npm install --save-dev @types/bcryptjs car ici on utilise TypeScript
+         */
+        finalData.password = await bcrypt.hash(data.password, 10);
+
+
+        // Si toutes les validations passent, on peut procéder à l'inscription de l'utilisateur
+        //
+        const saveUser = await db.collection('users').insertOne(finalData as Omit<IRegistrationData, '_id'>);
+
+        // Si l'inscription est réussie, on retourne une réponse JSON avec un message de succès et un statut 201 (Created)
+        if (saveUser.insertedId) {
+            return NextResponse.json({message: "Inscription réussie", userId: saveUser.insertedId}, {status: 201});
+        }
+
+    } catch (error) {
+        // En cas d'erreur, on retourne une réponse JSON avec un statut 500 (Internal Server Error)
+        return NextResponse.json({error: "Une erreur est survenue pendant l'inscription"}, {status: 500});
+    }
+}
