258b3d64b3b5fa0d2adaa28160c70aee15ec2c76
mika <wursthorn.mika@gmail.com>
2026-02-16 11:41:42 +0100

    feat: create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/**/*.{js,ts,jsx,tsx,mdx}", // Au cas où tu utilises un dossier src
+    ],
+    theme: {
+        extend: {
+            colors: {
+                meetme: {
+                    blue: '#3498db',
+                    dark: '#2980b9',
+                    green: '#82c91e',
+                    light: '#f0f9ff',
+                },
+            },
+        },
+    },
+    plugins: [],
+}
