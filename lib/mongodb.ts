e8e3edbe97ec43e55913da3046202082760cd167
mika <wursthorn.mika@gmail.com>
2026-04-24 10:09:44 +0200

    fix: mongodb.ts

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -5,32 +5,28 @@
  * Lance une erreur si l'URI n'est pas défini dans les variables d'environnement
  * Export une fonction connectToDB pour établir la connexion à la base de données
  */
-// Importation du client MongoDB depuis npm install mongodb
 import { MongoClient } from 'mongodb';
 
-// Déclaration d'une variable globale pour stocker la promesse de connexion MongoDB
+// déclare une variable globale pour stocker la promesse de connexion MongoDB
 declare global {
     var _mongoClientPromise: Promise<MongoClient> | undefined;
 }
 
-// Récupération de l'URI de connexion à MongoDB depuis les variables d'environnement
-const uri = process.env.MONGODB_URI;
-if (!uri) throw new Error('MONGODB_URI manquant');
-
-// Création d'une instance du client MongoDB
-const client = new MongoClient(uri);
-// Déclaration d'une variable pour stocker la promesse de connexion MongoDB
-let clientPromise: Promise<MongoClient>;
-
-// On utilise APP_ENV pour déterminer si on est en environnement de production ou de développement
-if (process.env.APP_ENV !== 'prod') {
-    if (!global._mongoClientPromise) {
-        global._mongoClientPromise = client.connect();
-    }
-    clientPromise = global._mongoClientPromise;
-} else {
-    clientPromise = client.connect();
+// crée une fonction pour établir la connexion à la base de données MongoDB
+function createClientPromise(): Promise<MongoClient> {
+    // récupère l'URI de connexion depuis les variables d'environnement
+    const uri = process.env.MONGODB_URI;
+    if (!uri) throw new Error('MONGODB_URI manquant');
+    return new MongoClient(uri).connect();
 }
 
-export default clientPromise;
+// exporte une fonction pour obtenir la promesse de connexion MongoDB
+export default function getClientPromise(): Promise<MongoClient> {
+    // si l'environnement d'exécution n'est pas en production, utilise une variable globale pour stocker la promesse
+    // de connexion MongoDB afin d'éviter de créer plusieurs connexions lors du développement
+    if (process.env.APP_ENV !== 'prod') {
+        return (global._mongoClientPromise ??= createClientPromise());
+    }
+    return createClientPromise();
+}
 
